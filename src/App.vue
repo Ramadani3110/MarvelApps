301134347a53691b675ae9e9abd3d167cd17c4f1
Rama <111574860+Ramadani3110@users.noreply.github.com>
2024-06-16 23:23:56 +0700

    feat: api for home page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Navbar/>
   <router-view/>
+  <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
-  name: 'navbar',
+  name: 'main',
   components: {
-    Navbar
+    Navbar,
+    Footer,
   }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,54 +18,71 @@
       </div>
     </div>
   </div>
-  <div class="pt-10 pb-5 px-20">
+  <div class="pt-10 pb-5 sm:px-20 px-5">
+
       <div class="flex justify-center flex-col px-4 py-16">
+
         <h1 class="text-center text-4xl font-bold">Features</h1>
         <div class="flex flex-row items-center justify-between">
           <h1 class="text-2xl font-bold py-5">Characters</h1>
           <router-link :to="{name: 'index.character'}">See All</router-link>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
           <!-- Card -->
-          <div class="card w-52 h-auto card-compact bg-gray-800 shadow-xl">
-            <figure><img class="w-full h-full object-cover" src="https://cdn.marvel.com/content/1x/3dman442.jpg" alt="Movie"/></figure>
+          <div class="card lg:w-52 md:w-48 sm:w-44 w-auto h-auto card-compact bg-gray-800 shadow-xl" v-for="(characters, index) in character" :key="index">
+            <figure>
+              <img class="w-full h-full object-cover" :src="characters.thumbnail.path+'.'+characters.thumbnail.extension" alt="Character">
+            </figure>
             <div class="card-body">
-              <h2 class="card-title">New movie is released!</h2>
+              <h2 class="card-title">{{characters.name}}</h2>
               <p>Click the button to watch on Jetflix app.</p>
             </div>
           </div>
-          <div class="card w-52 h-auto card-compact bg-gray-800 shadow-xl">
-            <figure><img class="w-full h-full object-cover" src="https://cdn.marvel.com/content/1x/triathlon.jpg" alt="Movie"/></figure>
-            <div class="card-body">
-              <h2 class="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-            </div>
+        </div>
+        <!-- Card -->
+      </div>
+      
+    <div class="flex justify-center flex-col px-4 py-16">
+
+      <div class="flex flex-row items-center justify-between">
+        <h1 class="text-2xl font-bold py-5">Comics</h1>
+        <router-link :to="{name: 'index.comic'}">See All</router-link>
+      </div>
+
+      <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div class="card lg:w-52 md:w-48 sm:w-44 w-auto h-auto card-compact bg-gray-800 shadow-xl" v-for="(comics, index) in comic" :key="index">
+          <figure>
+            <img class="w-full h-full object-cover" :src="comics.thumbnail.path+'.'+comics.thumbnail.extension" alt="Character">
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">{{comics.title}}</h2>
+            <p>Click the button to watch on Jetflix app.</p>
           </div>
-          <div class="card w-52 h-auto card-compact bg-gray-800 shadow-xl">
-            <figure><img class="w-full h-full object-cover" src="https://cdn.marvel.com/content/1x/354alk_com_crd_01.png" alt="Movie"/></figure>
-            <div class="card-body">
-              <h2 class="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-            </div>
-          </div>
-          <div class="card w-52 h-auto card-compact bg-gray-800 shadow-xl">
-            <figure><img class="w-full h-full object-cover" src="https://cdn.marvel.com/content/1x/354alk_com_crd_01.png" alt="Movie"/></figure>
-            <div class="card-body">
-              <h2 class="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-            </div>
-          </div>
-          <div class="card w-52 h-auto card-compact bg-gray-800 shadow-xl">
-            <figure><img class="w-full h-full object-cover" src="https://cdn.marvel.com/content/1x/354alk_com_crd_01.png" alt="Movie"/></figure>
-            <div class="card-body">
-              <h2 class="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-            </div>
-          </div>
-          <!-- Card -->
         </div>
       </div>
+    </div>
+
+    <div class="flex justify-center flex-col px-4 py-16">
+
+      <div class="flex flex-row items-center justify-between">
+        <h1 class="text-2xl font-bold py-5">Series</h1>
+        <router-link :to="{name: 'index.series'}">See All</router-link>
+      </div>
+
+      <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div class="card lg:w-52 md:w-48 sm:w-44 w-auto h-auto card-compact bg-gray-800 shadow-xl" v-for="(seriess, index) in series" :key="index">
+          <figure>
+            <img class="w-full h-full object-cover" :src="seriess.thumbnail.path+'.'+seriess.thumbnail.extension" alt="Character">
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">{{seriess.title}}</h2>
+            <p>{{ seriess.startYear }} - {{ seriess.endYear }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -83,13 +100,39 @@
 </style>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  setup(){
+    const character = ref([])
+    const comic = ref([])
+    const series = ref([])
+    onMounted( () => {
+      axios.get(`http://gateway.marvel.com/v1/public/characters?apikey=${process.env.VUE_APP_API_KEY}&ts=${process.env.VUE_APP_TS}&hash=${process.env.VUE_APP_HASH}&limit=4`).then((result) => {
+        character.value = result.data.data.results
+      }).catch((err) => {
+        console.log(err);
+      })
+
+      axios.get(`http://gateway.marvel.com/v1/public/comics?apikey=${process.env.VUE_APP_API_KEY}&ts=${process.env.VUE_APP_TS}&hash=${process.env.VUE_APP_HASH}&limit=4`).then((result) => {
+        comic.value = result.data.data.results
+      }).catch((err) => {
+        console.log(err);
+      })
+
+      axios.get(`http://gateway.marvel.com/v1/public/series?apikey=${process.env.VUE_APP_API_KEY}&ts=${process.env.VUE_APP_TS}&hash=${process.env.VUE_APP_HASH}&limit=4`).then((result) => {
+        series.value = result.data.data.results
+      }).catch((err) => {
+        console.log(err);
+      })
+      
+    })
+    return {
+      character,
+      comic,
+      series
+    }
   }
 }
 </script>
