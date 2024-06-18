7605dc51659302ef4b372c1da700d80ae77eab75
Rama <111574860+Ramadani3110@users.noreply.github.com>
2024-06-18 12:26:38 +0700

    feat: series index

--- src/views/Series/Index.vue
+++ src/views/Series/Index.vue
@@ -8,6 +8,34 @@
             </div>
         </div>
     </div>
+    <div class="pt-10 pb-5 sm:px-20 px-5">
+        <div class="flex justify-center flex-col px-4 py-16">
+
+            <h1 class="text-center text-4xl font-bold pb-10">All Series</h1>
+            <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+                <div class="card lg:w-52 md:w-48 sm:w-44 w-auto h-auto overflow-hidden card-compact bg-gray-800 shadow-xl" v-for="(seriesData,index) in series" :key="index">
+                    <router-link :to="{name: 'detail.series', params:{id:seriesData.id}}">
+                        <figure>
+                            <img class="w-full h-full object-cover" :src="seriesData.thumbnail.path+'.'+seriesData.thumbnail.extension" alt="Character">
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">{{ seriesData.title }}</h2>
+                            <p>{{ seriesData.startYear }} - {{ seriesData.endYear }}</p>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="flex justify-center flex-row items-center gap-2 px-4 py-16">
+            <button class="btn btn-info" @click="prevPage" :disabled="currentPage === 1">Previous</button>
+            <span>Page {{currentPage}} of {{totalPages}}</span>
+            <button class="btn btn-info" @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+        </div>
+        
+    </div>
+
 </template>
 
 <style scoped>
@@ -21,3 +49,56 @@
   filter: brightness(0.5) contrast(1.2) grayscale(0.2);
 }
 </style>
+
+<script>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+export default{
+    setup(){
+        const series = ref([]);
+        const currentPage = ref(1);
+        const offset = ref(0);
+        const totalPages = ref(0);
+        const limit = ref(10)
+
+        onMounted(() => {
+            axios.get(`http://gateway.marvel.com/v1/public/series?apikey=${process.env.VUE_APP_API_KEY}&ts=${process.env.VUE_APP_TS}&hash=${process.env.VUE_APP_HASH}&offset=${offset.value}&limit=${limit.value}`)
+            .then((result) => {
+                series.value = result.data.data.results
+                totalPages.value = Math.ceil(result.data.data.total / limit.value)
+            }).catch((err) => {
+                console.log(err);
+            })
+        })
+
+        const nextPage = () => {
+            offset.value += limit.value 
+            currentPage.value++
+            fetchCharacters()
+        }
+
+        const prevPage = () => {
+            offset.value -= limit.value
+            currentPage.value--
+            fetchCharacters()
+        }
+
+        const fetchCharacters = () => {
+            axios.get(`http://gateway.marvel.com/v1/public/series?apikey=${process.env.VUE_APP_API_KEY}&ts=${process.env.VUE_APP_TS}&hash=${process.env.VUE_APP_HASH}&limit=${limit.value}&offset=${offset.value}`)
+            .then((result) => {
+                series.value = result.data.data.results;
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+
+        return {
+            series,
+            currentPage,
+            totalPages,
+            nextPage,
+            prevPage
+        }
+    }
+}
+</script>
